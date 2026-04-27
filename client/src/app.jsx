f79a33a71a7d744a3588a04d35741a2852d79d74
Ayush Bhandari <ayushbhandari00007@gmail.com>
2026-04-27 15:42:13 +0530

    moved files to components folder and fixed imports

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Navbar from "./components/navbar";
+import Form from "./components/form";
+import Preview from "./components/preview";
+import TemplateGallery from "./components/templategallery";
 import { 
   Rocket, 
   ArrowLeft, 
